2d56bc8fb321c9db3a95bb59bbd230bd56818ec8
Sanil14 <saniljalan14@gmail.com>
2021-10-31 19:22:54 +0000

    rework js files

--- js/global.js
+++ js/global.js
@@ -1,4 +1,4 @@
-$(document).ready(() => {
+$(() => {
 
     new WOW().init();
 
--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
-$(document).ready(() => {
-
-    new WOW().init();
+$(() => {
 
     var options = {
         strings: ["Aspiring Web Developer", "Aspiring NodeJS Developer", "Aspiring Backend Developer", "Aspiring Software Engineer"],
@@ -12,32 +10,4 @@ $(document).ready(() => {
     };
 
     new Typed(".namecenter h1", options);
-
-    $(".openbtn").click(function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        if ($(".sidepanel").height() > 0) {
-            hideNav();
-        } else {
-            $(".sidepanel").css('height', '100vh');
-            $(this).addClass('open')
-        }
-    })
-
-    $(".sidepanel").click(function (e) {
-        e.stopPropagation();
-    })
-
-    $(document).click(function () {
-        hideNav();
-    })
-
-    $(".sidepanel .closebtn").click(function () {
-        hideNav();
-    })
-
-    function hideNav() {
-        $(".sidepanel").css('height', '0px');
-        $(".openbtn").removeClass('open')
-    }
 })
